de02adfd98d6e649fc829b85516718857d28d46f
kiplangat01 <97243798+kiplangat01@users.noreply.github.com>
2022-03-25 03:42:52 +0300

    finished on polishing up the page

--- js/home.js
+++ js/home.js
@@ -1,39 +1,39 @@
 $(document).ready(function(){
 
   $("#book1").click(function(){
-      alert("thank you for choosing sarova stanley fill aut the form to get linked")
+      alert("thank you for choosing sarova stanley fill out the form to get linked")
   })
 
   $("#book2").click(function(){
-    alert("thank you for choosing vilarosa kempinski fill aut the form to get linked")
+    alert("thank you for choosing vilarosa kempinski fill 0ut the form to get linked")
 })
 
 $("#book3").click(function(){
-    alert("thank you for choosing  Ole sereni fill aut the form to get linked")
+    alert("thank you for choosing  Ole sereni fill out the form to get linked")
 })
 
 $("#book4").click(function(){
-    alert("thank you for choosing Shalodin queens fill aut the form to get linked")
+    alert("thank you for choosing Shalodin queens fill out the form to get linked")
 })
 
 $("#book5").click(function(){
-    alert("thank you for choosing English point fill aut the form to get linked")
+    alert("thank you for choosing English point fill out the form to get linked")
 })
 
 $("#book6").click(function(){
-    alert("thank you for choosing flamingo serena fill aut the form to get linked")
+    alert("thank you for choosing flamingo serena fill out the form to get linked")
 })
 
 $("#book7").click(function(){
-    alert("thank you for choosing Camp david park fill aut the form to get linked")
+    alert("thank you for choosing Camp david park fill out the form to get linked")
 })
 
 $("#book8").click(function(){
-    alert("thank you for choosing Bamburi beach hotel fill aut the form to get linked")
+    alert("thank you for choosing Bamburi beach hotel fill out the form to get linked")
 })
 
 $("#book9").click(function(){
-    alert("thank you for choosing Mau gate resort fill aut the form to get linked")
+    alert("thank you for choosing Mau gate resort fill out the form to get linked")
 })
 
 
